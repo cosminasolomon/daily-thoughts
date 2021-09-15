fd76b4570f88a80921959608c0a3363356c20d03
Cosmina Solomon <solomoncosminamaria@gmail.com>
2021-09-15 16:50:32 +0200

    authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends React.Component {
-  component() {
-    const apiUrl = `https://daily-thoughts-app.herokuapp.com/api/register/`;
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+class App extends Component {
   render() {
-    return <div>Connection</div>;
+    // ...
   }
 }
+
 export default App;
+
+// import React from "react";
+
+// class App extends React.Component {
+//   component() {
+//     const apiUrl = `https://daily-thoughts-app.herokuapp.com/api/register/`;
+//     fetch(apiUrl)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+//   }
+//   render() {
+//     return <div>Connection</div>;
+//   }
+// }
+// export default App;
