3b796b337fb2481a587cb631c3ce92a412a93eb9
Cosmina Solomon <solomoncosminamaria@gmail.com>
2021-09-17 17:02:08 +0200

    added posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+// import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
@@ -36,8 +36,10 @@ const useStyles = makeStyles((theme) => ({
 
 const Posts = (props) => {
   const { posts } = props;
+  console.log(props);
   const classes = useStyles();
   if (!posts || posts.length === 0) return <p>Can not find any posts, sorry</p>;
+
   return (
     <React.Fragment>
       <Container maxWidth="md" component="main">
@@ -47,11 +49,11 @@ const Posts = (props) => {
               // Enterprise card is full width at sm breakpoint
               <Grid item key={post.id} xs={12} md={4}>
                 <Card className={classes.card}>
-                  <CardMedia
+                  {/* <CardMedia
                     className={classes.cardMedia}
                     image="https://source.unsplash.com/random"
                     title="Image title"
-                  />
+                  /> */}
                   <CardContent className={classes.cardContent}>
                     <Typography
                       gutterBottom
@@ -62,12 +64,8 @@ const Posts = (props) => {
                       {post.title.substr(0, 50)}...
                     </Typography>
                     <div className={classes.postText}>
-                      <Typography
-                        component="p"
-                        color="textPrimary"
-                      ></Typography>
-                      <Typography variant="p" color="textSecondary">
-                        {post.excerpt.substr(0, 60)}...
+                      <Typography color="textSecondary">
+                        {post.content.substr(0, 60)}...
                       </Typography>
                     </div>
                   </CardContent>
